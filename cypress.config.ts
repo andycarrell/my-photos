c58bfab3e9312ec0212f00c001f663adafc3f9c2
andycarrell <carrellandrew@gmail.com>
2022-09-03 20:04:34 +1200

    Update prettier & apply formatting

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: "fwkmco",
   e2e: {
-    baseUrl: "http://localhost:3000"
+    baseUrl: "http://localhost:3000",
   },
 });
